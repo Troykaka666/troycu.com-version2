eb6727d5ddc8c1c8516a5b290a4e1d3176975f50
Troy <Troywu9527@gmail.com>
2019-03-20 22:54:23 -0400

    finished project frontend part, and improve responsive design for homepage profile and project section

--- temp/js/index.js
+++ temp/js/index.js
@@ -2,13 +2,12 @@ var $ = require('jquery');
 
 
 $(document).ready(function(){
-    $('.button-download').mouseenter(function(){
+    $('.button-download').mouseenter(() => {
         $('.button__item').addClass('top');
     });
     $('.button-download').mouseleave(function(){
         $('.button__item').removeClass('top');
     });
-
 
 
 });
